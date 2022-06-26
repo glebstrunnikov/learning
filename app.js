03c3f8f0d4f1a5a5a5c3e40a48153e698ce73c2d
Gleb Strunnikov <glebstrunnikov@gmail.com>
2022-06-26 17:47:31 +0300

    initializing project files

---
+++ app.js
@@ -0,0 +1,54 @@
+let gameOver;
+
+class Putin {
+    constructor() {
+    };
+    static poops = [];
+
+    static generatePoo() {
+        const newPoo = new Poo();
+        const newLine = document.createElement('tr');
+        newLine.innerHTML = `
+        <td>${newPoo.msg}</td>
+        `
+        document.querySelector('#pooList').appendChild(newLine);
+        this.poops.push(newPoo.type);
+    };
+
+    static noPooCheck() {
+        const last3Poops = this.poops.slice(-3);
+        // if (last3Poops === [0,0,0]) {console.log('BOOM')}; // тут я хотел сравнить эррей с эрреем, но почему-то оно не работает. Почему?
+        if (last3Poops.at(-1) === 0 && last3Poops.at(-2) === 0 && last3Poops.at(-3) === 0) {
+            const finalLine = document.createElement('tr');
+            finalLine.innerHTML = `
+            <td>Putin exploded! The end.</td>
+            `
+            document.querySelector('#pooList').appendChild(finalLine);
+            gameOver = true
+        }
+    };
+};
+
+document.querySelector('#pooBtn').addEventListener('click', poo);
+
+function poo(e) {
+    e.preventDefault();
+    if (gameOver != true) {
+        Putin.generatePoo();
+        Putin.noPooCheck();
+        }
+};
+
+class Poo {
+    constructor() {
+        this.type = Math.floor(Math.random()*8+1);
+        // 1 - diarrhea, 2 - solid, 3 - mixed, 4-8 - no poop
+        if (this.type === 1) {this.msg = 'Putin pooped with diarrhea...'};
+        if (this.type === 2) {this.msg = 'Putin pooped with solid poop...'};
+        if (this.type === 3) {this.msg = 'Putin pooped with mixed poop...'};
+        if (this.type >=4 && this.type <= 8) {
+            this.type = 0;
+            this.msg = "Putin can't poop today, oh gosh..."
+        };
+    }
+}
