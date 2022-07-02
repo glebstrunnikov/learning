a399be21c83d1bb15f22ac93bb973640ec95e84d
Gleb Strunnikov <glebstrunnikov@gmail.com>
2022-07-02 19:19:36 +0300

    updated easyhttp with async-await

--- easyhttp (modified - fetch, classes)/app.js
+++ easyhttp (modified - fetch, classes)/app.js
@@ -5,7 +5,7 @@ http.get('https://jsonplaceholder.typicode.com/posts')
   .then(res => console.log(res))
   .catch(err => console.log(err))
 
-// // Get Single Post
+// // // Get Single Post
 http.get('https://jsonplaceholder.typicode.com/posts/1')
   .then(res => console.log(res))
   .catch(err => console.log(err))
@@ -26,7 +26,7 @@ http.put('https://jsonplaceholder.typicode.com/posts/5', data)
   .then(res => console.log(res))
   .catch(err => console.log(err))
 
-// // // Delete Post
+// // // // Delete Post
 http.delete('https://jsonplaceholder.typicode.com/posts/1')
   .then(res => console.log(res))
   .catch(err => console.log(err))
--- easyhttp (modified - fetch, classes)/easyhttp.js
+++ easyhttp (modified - fetch, classes)/easyhttp.js
@@ -5,51 +5,88 @@ class EasyHTTP {
     else return res;
   }
 
-  get(url) {
-    return new Promise((resolve, reject) => {
-          fetch(url)
-          .then(res => this.checkErr(res))
-          .then(res => res.json())
-          .then(jsonText => resolve(jsonText))
-          .catch((err => reject(err)));
-      }
-    )
+  async get(url) {
+    const response = await fetch(url);
+    this.checkErr(response);
+    const responseText = await response.text();
+    return responseText
   }
 
-  post(url, data) {
-    return new Promise ((resolve, reject) =>
-      fetch(url, {
+  async post(url, data) {
+    const response = await fetch(url, {
         method: 'POST',
         headers: {'Content-type': 'application/json'},
         body: JSON.stringify(data)
-      })
-      .then(res => this.checkErr(res))
-      .then(res => resolve(`Data posted! Here's the response: ${res}`))
-      .catch(err => reject(err))
-    )
+      });
+      this.checkErr(response);
+      const responseText = await `Post published! Response: ${response.text()}`;
+      return responseText
   }
   
-  put(url, data) {
-    return new Promise((resolve, reject) =>
-      fetch(url, {
+  async put(url, data) {
+    const response = await fetch(url, {
         method: 'PUT',
         headers: {'Content-type': 'application/json'},
         body: JSON.stringify(data)
       })
-      .then(res => this.checkErr(res))
-      .then(res => resolve(`Data put! Here's the response: ${res}`))
-      .catch(err => reject(err))
-    )
+      this.checkErr(response);
+      const responseText = await `Post updated! Response: ${response.text()}`;
+      return responseText
   }
 
-  delete(url) {
-    return new Promise((resolve, reject) =>
-      fetch(url, {
-        method: 'DELETE'
-      })
-      .then(res => this.checkErr(res))
-      .then(() => resolve(`Post deleted!`))
-      .catch(err => reject(err))
-    )
+  async delete(url) {
+    const response = await fetch(url, {method: 'DELETE'});
+    this.checkErr(response);
+    return 'Post deleted!'
   }
+
+//   delete(url) {
+//     return new Promise((resolve, reject) =>
+//       fetch(url, {
+//         method: 'DELETE'
+//       })
+//       .then(res => this.checkErr(res))
+//       .then(() => resolve(`Post deleted!`))
+//       .catch(err => reject(err))
+//     )
+//   }
 }
+
+
+
+//   put(url, data) {
+//     return new Promise((resolve, reject) =>
+//       fetch(url, {
+//         method: 'PUT',
+//         headers: {'Content-type': 'application/json'},
+//         body: JSON.stringify(data)
+//       })
+//       .then(res => this.checkErr(res))
+//       .then(res => resolve(`Data put! Here's the response: ${res}`))
+//       .catch(err => reject(err))
+//     )
+//   }
+
+//   delete(url) {
+//     return new Promise((resolve, reject) =>
+//       fetch(url, {
+//         method: 'DELETE'
+//       })
+//       .then(res => this.checkErr(res))
+//       .then(() => resolve(`Post deleted!`))
+//       .catch(err => reject(err))
+//     )
+//   }
+
+      // .then(res => res.json)
+      // .then(jsonText => console.log(jsonText))
+      // .catch(err => console.log(err))
+
+    // return new Promise((resolve, reject) => {
+    //       fetch(url)
+    //       .then(res => this.checkErr(res))
+    //       .then(res => res.json())
+    //       .then(jsonText => resolve(jsonText))
+    //       .catch((err => reject(err)));
+    //   }
+    // )
