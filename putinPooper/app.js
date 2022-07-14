5ec23821347983e43051a6743a1be8000c4d87be
Gleb Strunnikov <glebstrunnikov@gmail.com>
2022-07-15 00:00:17 +0300

    added loading poop history from a text file in PutinPooper

--- putinPooper/app.js
+++ putinPooper/app.js
@@ -1,22 +1,31 @@
 let gameOver;
 
 class Putin {
-    constructor() {
-    };
-    static poops = [];
+    generatePoo(pooType) {
+        // 1 - diarrhea, 2 - solid, 3 - mixed, 4-8 - no poop
+        const newPoo = new Poo(pooType);
+        if (newPoo.type === 1) {newPoo.msg = 'Putin pooped with diarrhea...'};
+        if (newPoo.type === 2) {newPoo.msg = 'Putin pooped with solid poop...'};
+        if (newPoo.type === 3) {newPoo.msg = 'Putin pooped with mixed poop...'};
+        if (newPoo.type >=4 && newPoo.type <= 8) {
+            newPoo.type = 0;
+            newPoo.msg = "Putin can't poop today, oh gosh..."
+        };
+        putin.drawPoo(newPoo.msg)
+        putin.poops.push(newPoo.type);
 
-    static generatePoo() {
-        const newPoo = new Poo();
+    };
+
+    drawPoo (msg) {
         const newLine = document.createElement('tr');
         newLine.innerHTML = `
-        <td>${newPoo.msg}</td>
+        <td>${msg}</td>
         `
         document.querySelector('#pooList').appendChild(newLine);
-        this.poops.push(newPoo.type);
-    };
+    }
 
     static noPooCheck() {
-        const last3Poops = this.poops.slice(-3);
+        const last3Poops = putin.poops.slice(-3);
         if (last3Poops.at(-1) === 0 && last3Poops.at(-2) === 0 && last3Poops.at(-3) === 0) {
             const finalLine = document.createElement('tr');
             finalLine.innerHTML = `
@@ -26,28 +35,54 @@ class Putin {
             gameOver = true
         }
     };
+
+    // static async savePoo() {
+    //     const response = await fetch('suitcase.txt', {
+    //         method: 'PUT',
+    //         headers: {'Content-type': 'application/json'},
+    //         body: '7,2,7,3,7,7,7'
+    //     })
+    //     const responseText = response.text();
+    //     responseText
+    //     .then(data => console.log(data + 'saved'))
+    // }
+
+    static async loadPoo() {
+        putin.poops = [];
+        document.querySelector('#pooList').innerHTML= ""
+        const pooListResponse = await fetch('suitcase.txt')
+        .then(res => res.text())
+        .then(data => {
+            const pooArray = data.split(',')
+            pooArray.forEach(el => {putin.generatePoo(parseInt(el))
+            Putin.noPooCheck()})
+        })
+    
+    }
+
 };
 
+const putin = new Putin; // приятно писать эту фамилию с маленькой буквы не из бессильной злобы, а со смыслом)
+putin.poops = []
+
 document.querySelector('#pooBtn').addEventListener('click', poo);
+// document.querySelector('#saveBtn').addEventListener('click', Putin.savePoo)
+document.querySelector('#loadBtn').addEventListener('click', Putin.loadPoo)
 
 function poo(e) {
     e.preventDefault();
     if (gameOver != true) {
-        Putin.generatePoo();
+        putin.generatePoo(Math.floor(Math.random()*8+1));
         Putin.noPooCheck();
         }
 };
 
 class Poo {
-    constructor() {
-        this.type = Math.floor(Math.random()*8+1);
-        // 1 - diarrhea, 2 - solid, 3 - mixed, 4-8 - no poop
-        if (this.type === 1) {this.msg = 'Putin pooped with diarrhea...'};
-        if (this.type === 2) {this.msg = 'Putin pooped with solid poop...'};
-        if (this.type === 3) {this.msg = 'Putin pooped with mixed poop...'};
-        if (this.type >=4 && this.type <= 8) {
-            this.type = 0;
-            this.msg = "Putin can't poop today, oh gosh..."
-        };
+    constructor(type) {
+        this.type = type
     }
 }
+
+// 1) Добавить в класс путин метод SAVE и соответственно кнопку во фронтенде, которая все результаты poop'а будет сохранять в файл в любом формате, я предлагаю 1 строчка - одна запись, внутри любые делиметеры (можно запятую, можно вертикальную палочку) по очереди поля
+
+// 2) Добавить метод LOAD и кнопку во фронте LOAD, которая будет подъедать этот файл и сразу выводить
