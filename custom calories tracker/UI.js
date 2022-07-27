82aa87d1f853c1ad78151cddf0e8468d120698da
Gleb Strunnikov <glebstrunnikov@gmail.com>
2022-07-28 00:13:40 +0300

    finished the core of calorie tracker, only a few features left

---
+++ custom calories tracker/UI.js
@@ -0,0 +1,156 @@
+productLib.forEach(element => {
+    const productFromLib = new Product(element.foodName, element.foodScale, element.foodKcal)
+    productFromLib.draw()
+}
+)
+
+UISaveProductBtn.addEventListener('click', saveProductToLib)
+function saveProductToLib(e) {
+    e.preventDefault();
+    const newProduct = new Product
+    newProduct.loadFromUI()
+
+    let duplicateCounter = 0
+    productLib.forEach(el => {
+        if (el.foodName != newProduct.name) {
+            duplicateCounter++
+            if (duplicateCounter === productLib.length) {
+                newProduct.addToLib()
+                newProduct.draw()
+                UIName.value = newProduct.name;
+            }
+        }
+    })
+    duplicateCounter = 0
+
+}
+
+UIDeleteBtn.addEventListener('click', deleteProduct)
+function deleteProduct () {
+    productLib.forEach(element => {
+        if (element.foodName.indexOf(UIProductName.value) != -1) {
+            console.log(productLib.indexOf(element))
+            productLib.splice(productLib.indexOf(element), 1)
+        }
+    }
+    )
+    UILibrary.innerHTML = '';
+    productLib.forEach(element => {
+        const productFromLib = new Product(element.foodName, element.foodScale, element.foodKcal)
+        productFromLib.draw()
+    }
+    )
+    localStorage.setItem('products', JSON.stringify(productLib))
+
+}
+
+document.querySelector('#editorBtn').addEventListener('click', toModal)
+function toModal () {
+    UIProductName.value = UIName.value
+    productLib.forEach(element => {
+        if (element.foodName.indexOf(UIProductName.value) != -1) {
+            UIProductKcal.value = element.foodKcal
+        }
+    })
+}
+
+UIName.addEventListener('input', dropdownUpdate)
+function dropdownUpdate() {
+    document.querySelector('#UIDropdownItems').innerHTML = ''
+    productLib.forEach(element => {
+        if (element.foodName.indexOf(UIName.value) != -1) {
+            const next1to5Li = document.createElement('li')
+            next1to5Li.innerHTML = `<a id="list${element.foodName}" class="dropdown-item" href="#">${element.foodName}</a>`
+            document.querySelector('#UIDropdownItems').appendChild(next1to5Li)
+            document.querySelector(`#list${element.foodName}`).addEventListener('click', toInput)
+            document.querySelector(`#list${element.foodName}`).addEventListener('click', checkIfCanEat)
+            function toInput () {
+                UIName.value = element.foodName
+                document.querySelector('#UIDropdownItems').innerHTML = ''
+            }
+        }
+    });
+}
+
+document.addEventListener('keyup', checkIfCanEat)
+document.addEventListener('click', checkIfCanEat)
+function checkIfCanEat () {
+    UIYummy.disabled = true;
+    UIKcal.placeholder = ''
+    productLib.forEach(element => {
+        if (UIName.value === element.foodName) {
+            UIYummy.disabled = false
+            UIKcal.placeholder = `${element.foodScale === 2 ? element.foodKcal*UIQuant.value : element.foodKcal*UIQuant.value/100} kcal`
+            element.foodScale === 1 ? UIScale.textContent = 'g' : UIScale.textContent = 'pc.'
+        }
+    })
+    if (UIQuant.value === '' || UIQuant.value === null || UIQuant.value === 0) {
+        UIYummy.disabled = true;
+        UIKcal.placeholder = ''
+    }
+}
+
+UIYummy.addEventListener('click', eat)
+function eat() {
+    let currentScale;
+    let currentKcal;
+    productLib.forEach(element => {
+        if (UIName.value === element.foodName) {
+            currentScale = element.foodScale;
+            currentKcal = element.foodKcal
+
+        }
+    })
+    const newMeal = new Meal(UIName.value, currentScale, currentKcal, UIQuant.value);
+    newMeal.eatSave();
+    newMeal.countTotalKcal();
+    drawTable(newMeal);
+}
+
+function drawTable (meal) {
+    const newMealLine = document.createElement('tr');
+    newMealLine.innerHTML = `
+    <th scope="row">${meal.name}</th>
+    <td>${meal.amount}</td>
+    <td>${meal.scale === 1 ? 'g' : 'pc.'}</td>
+    <td>${meal.scale === 2 ? meal.amount*meal.kcal : meal.amount*meal.kcal/100
+    }</td>
+    <td><button id="del_${meal.name}_${meal.amount}" type="button" class="btn-close" aria-label="Close"></button></td>
+    `
+    UIList.appendChild(newMealLine)
+    document.getElementById(`del_${meal.name}_${meal.amount}`).addEventListener('click', deleteLine)
+    function deleteLine(e) {
+        e.preventDefault;
+        e.target.parentElement.parentElement.remove()
+        belly.forEach(element => {
+            if (element.foodName === meal.name && element.foodAmount === meal.amount) {
+                belly.splice(belly.indexOf(element), 1)
+            }
+        })
+        localStorage.setItem('belly', JSON.stringify(belly))
+        meal.countTotalKcal()
+    }
+}
+
+belly.forEach(element => {
+    const mealToDraw = new Meal(element.foodName, element.foodScale, element.foodKcal, element.foodAmount)
+    mealToDraw.countTotalKcal();
+    drawTable(mealToDraw)
+
+})
+
+UIClear.addEventListener('click', clearAll)
+function clearAll() {
+    if (UIClear.textContent === 'Clear All') {UIClear.firstElementChild.textContent = "Yes I'm sure"}
+    else if (UIClear.firstElementChild.textContent === "Yes I'm sure") {
+    belly = [];
+    localStorage.setItem('belly', JSON.stringify(belly));
+    productLib = [];
+    localStorage.setItem('products', JSON.stringify(productLib));
+    UIList.innerHTML = '';
+    UILibrary.innerHTML = '';
+    totalKcal = 0;
+    UITotalKcal.textContent = 0
+    UIClear.firstElementChild.textContent = 'Clear All'
+    }
+}
--- custom calories tracker/main.js
+++ custom calories tracker/main.js
@@ -1,5 +1,14 @@
 let productLib = JSON.parse(localStorage.getItem('products'))
 if (productLib === null) {productLib = []}
+let belly = JSON.parse(localStorage.getItem('belly'))
+if (belly === null) {belly = []}
+let totalKcal = 0
+
+const UIName = document.getElementById('UIName');
+const UIQuant = document.getElementById('UIQuant');
+const UIKcal = document.getElementById('UIKcal');
+const UIScale = document.getElementById('UIScale');
+const UIYummy = document.getElementById('UIYummy');
 
 const UIProductName = document.getElementById('UIProductName');
 const UIProductScale1 = document.getElementById('UIProductScale1');
@@ -8,6 +17,11 @@ const UIProductKcal = document.getElementById('UIProductKcal');
 const UIDeleteBtn = document.getElementById('UIDeleteBtn');
 const UICloseBtn = document.getElementById('UICloseBtn');
 const UISaveProductBtn = document.getElementById('UISaveProductBtn');
+
+const UIList = document.getElementById('UIList');
+const UITotalKcal = document.getElementById('UITotalKcal')
+const UILibrary = document.getElementById('UILibrary')
+const UIClear = document.getElementById('UIClear')
 
 
 class Product{
@@ -22,34 +36,74 @@ class Product{
         localStorage.setItem('products', JSON.stringify(productLib))
     }
 
+    loadFromUI () {
+        this.name = UIProductName.value
+        this.scale = (UIProductScale1.checked === true ? 1 : 2)
+        this.kcal = UIProductKcal.value
+    }
+
+    draw () {
+        const newBadge = document.createElement('button');
+        newBadge.type = 'button';
+        newBadge.classList = "btn btn-success mb-1"
+        newBadge.id = `badge${this.name}`
+        newBadge.textContent = `${this.name}`;
+
+        UILibrary.appendChild(newBadge);
+        document.querySelector(`#badge${this.name}`).addEventListener('click', writeToUI)
+        const self = this
+        function writeToUI () {
+            UIName.value = self.name
+            UIScale.value = self.scale
+        }
+    }
+}
+
+class Meal extends Product {
+    constructor (name, scale, kcal, amount) {
+    super(name, scale, kcal);
+    this.amount = amount
+    }
+
+    eatSave() {
+        belly.push({foodName: this.name, foodScale: this.scale, foodKcal: this.kcal, foodAmount: this.amount})
+        localStorage.setItem('belly', JSON.stringify(belly))
+    }
+
+    countTotalKcal () {
+        totalKcal = 0
+        belly.forEach(element => {
+            if (element.foodScale === 2) {
+            totalKcal += parseInt(element.foodAmount)*parseInt(element.foodKcal)
+            } else totalKcal += (parseInt(element.foodAmount)*parseInt(element.foodKcal))/100
+        })
+        UITotalKcal.textContent = totalKcal
+    }
 }
 
 const foodExample = new Product('Apple', 2, 50)
 const foodExample2 = new Product('Rice', 1, 130)
-console.log(foodExample)
-// foodExample.addToLib()
-// foodExample2.addToLib()
-console.log(productLib)
+const foodExample3 = new Product('Apricote', 2, 60)
+const foodExample4 = new Product('Bread', 1, 100)
+const foodExample5 = new Product('Beer', 2, 225)
 
+const mealExample = new Meal(foodExample.name, foodExample.scale, foodExample.kcal, 1)
 
-
-class Modal{ 
-    save(name, scale, kcal) {
-        this.name = UIProductName.value
-        this.scale = (UIProductScale1.checked === true ? 1 : 2)
-        this.kcal = UIProductKcal.value
-        const newProduct = new Product(this.name, this.scale, this.kcal)
-        newProduct.addToLib()
-    }
-}
-const modal = new Modal
-console.log(modal)
-
-UISaveProductBtn.addEventListener('click', saveProductToLib)
-
-function saveProductToLib(e) {
-    e.preventDefault();
-    modal.save()
+if (productLib.length <5) {
+    foodExample.addToLib()
+    foodExample2.addToLib()
+    foodExample3.addToLib()
+    foodExample4.addToLib()
+    foodExample5.addToLib()
 }
 
-document.querySelector('#editorBtn').add...
+// To do:
+// + 0. Rewrite adding lines to table to make it a single function
+// + 1. Delete from list buttons
+// + 2. Duplicate in lib preventor
+// 3. Save day to archive
+// 4. Archive page
+// 5. Error msgs
+// 6. Reorder to classes and docs
+// 7. Make case insensitive
+
