2c994d61202e97637c748a22d245795483f97fac
Gleb Strunnikov <glebstrunnikov@gmail.com>
2022-10-18 21:40:42 +0300

    Putin now poops into an SQLite DB on an Express server

--- putinPooper/app.js
+++ putinPooper/app.js
@@ -1,103 +1,116 @@
 let gameOver;
-let suitcaseOpen = false
-const suitcase = document.querySelector('#suitcase-field')
+const PORT = 3000;
 
 class Putin {
-    generatePoo(pooType) {
-        // 1 - diarrhea, 2 - solid, 3 - mixed, 4-8 - no poop
-        const newPoo = new Poo(pooType);
-        if (newPoo.type === 1) {newPoo.msg = 'Putin pooped with diarrhea...'};
-        if (newPoo.type === 2) {newPoo.msg = 'Putin pooped with solid poop...'};
-        if (newPoo.type === 3) {newPoo.msg = 'Putin pooped with mixed poop...'};
-        if (newPoo.type === 0 || newPoo.type >=4 && newPoo.type <= 8) {
-            newPoo.type = 0;
-            newPoo.msg = "Putin can't poop today, oh gosh..."
-        };
-        putin.drawPoo(newPoo.msg)
-        putin.poops.push(newPoo.type);
+  generatePoo(pooType) {
+    // 1 - diarrhea, 2 - solid, 3 - mixed, 4-8 - no poop
+    const newPoo = new Poo(pooType);
+    if (newPoo.type === 1) {
+      newPoo.msg = "Putin pooped with diarrhea...";
+    }
+    if (newPoo.type === 2) {
+      newPoo.msg = "Putin pooped with solid poop...";
+    }
+    if (newPoo.type === 3) {
+      newPoo.msg = "Putin pooped with mixed poop...";
+    }
+    if (newPoo.type === 0 || (newPoo.type >= 4 && newPoo.type <= 8)) {
+      newPoo.type = 0;
+      newPoo.msg = "Putin can't poop today, oh gosh...";
+    }
+    putin.drawPoo(newPoo.msg);
+    putin.poops.push(newPoo.type);
+  }
 
-    };
+  putToSuitcase(pooType) {
+    fetch(`http://localhost:${PORT}/poop`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ pooType: pooType }),
+    });
+    console.log(JSON.stringify(pooType));
+  }
 
-    drawPoo (msg) {
-        const newLine = document.createElement('tr');
-        newLine.innerHTML = `
+  drawPoo(msg) {
+    const newLine = document.createElement("tr");
+    newLine.innerHTML = `
         <td>${msg}</td>
-        `
-        document.querySelector('#pooList').appendChild(newLine);
-    }
+        `;
+    document.querySelector("#pooList").appendChild(newLine);
+  }
 
-    static noPooCheck() {
-        const last3Poops = putin.poops.slice(-3);
-        if (last3Poops.at(-1) === 0 && last3Poops.at(-2) === 0 && last3Poops.at(-3) === 0) {
-            const finalLine = document.createElement('tr');
-            finalLine.innerHTML = `
+  updateSuitcase(poops) {
+    this.poops = poops;
+    document.querySelector("#pooList").innerHTML = "";
+    poops.forEach((el) => this.generatePoo(el));
+    Putin.noPooCheck();
+  }
+
+  static clearSuitcase() {
+    this.poops = [];
+    document.querySelector("#pooList").innerHTML = "";
+    fetch(`http://localhost:${PORT}/reset`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: null,
+    });
+  }
+
+  static noPooCheck() {
+    const last3Poops = putin.poops.slice(-3);
+    if (
+      last3Poops.at(-1) === 0 &&
+      last3Poops.at(-2) === 0 &&
+      last3Poops.at(-3) === 0
+    ) {
+      const finalLine = document.createElement("tr");
+      finalLine.innerHTML = `
             <td>Putin exploded! The end.</td>
-            `
-            document.querySelector('#pooList').appendChild(finalLine);
-            gameOver = true
-        }
-    };
-
-    savePoo (data) {
-        const pooHistory = data.join()
-        document.querySelector('#pooHistory').textContent = pooHistory
-        console.log(data)
+            `;
+      document.querySelector("#pooList").appendChild(finalLine);
+      gameOver = true;
     }
+  }
 
-    loadPoo (data) {
-        const pooHistory = data.split(',');
-        putin.poops = [];
-        document.querySelector('#pooList').innerHTML= "";
-        console.log(pooHistory)
-        pooHistory.forEach(el => {
-            putin.generatePoo(parseInt(el))
-            Putin.noPooCheck()});
-    }
-
-};
-
-const putin = new Putin; // приятно писать эту фамилию с маленькой буквы не из бессильной злобы, а со смыслом)
-putin.poops = []
-
-document.querySelector('#pooBtn').addEventListener('click', poo);
-document.querySelector('#suitcaseBtn').addEventListener('click', toggleSuitcase)
-document.querySelector('#saveBtn').addEventListener('click', save)
-document.querySelector('#loadBtn').addEventListener('click', load)
-
-function load () {
-    const pooToLoad = document.querySelector('#pooHistory').value
-    putin.loadPoo(pooToLoad)
+  static checkPooHistory() {
+    fetch(`http://localhost:${PORT}/poops`)
+      .then((res) => res.json())
+      .then((res) => {
+        res.forEach((el) => {
+          putin.poops.push(el.pooType);
+          console.log(putin.poops);
+        });
+        putin.updateSuitcase(putin.poops);
+      });
+  }
 }
 
-function save () {
-    putin.savePoo(putin.poops)
-}
+const putin = new Putin(); // приятно писать эту фамилию с маленькой буквы не из бессильной злобы, а со смыслом)
+putin.poops = [];
+Putin.checkPooHistory();
 
-// document.querySelector('#loadBtn').addEventListener('click', Putin.loadPoo)
-
-function toggleSuitcase (e) {
-    e.preventDefault();
-    if (suitcaseOpen === false) {
-        suitcase.style = "display: block"
-        suitcaseOpen = true}
-    else if (suitcaseOpen === true) {
-        suitcase.style = "display: none"
-        suitcaseOpen = false}
-}
+document.querySelector("#pooBtn").addEventListener("click", poo);
+document
+  .querySelector("#resetBtn")
+  .addEventListener("click", Putin.clearSuitcase);
 
 function poo(e) {
-    e.preventDefault();
-    if (gameOver != true) {
-        putin.generatePoo(Math.floor(Math.random()*8+1));
-        Putin.noPooCheck();
-        }
-};
+  e.preventDefault();
+  if (gameOver != true) {
+    const pooType = Math.floor(Math.random() * 8 + 1);
+    putin.generatePoo(pooType);
+    putin.putToSuitcase(pooType);
+    Putin.noPooCheck();
+  }
+}
 
 class Poo {
-    constructor(type) {
-        this.type = type
-    }
+  constructor(type) {
+    this.type = type;
+  }
 }
+
+console.log(Putin.prototype);
 
 // 1) Добавить в класс путин метод SAVE и соответственно кнопку во фронтенде, которая все результаты poop'а будет сохранять в файл в любом формате, я предлагаю 1 строчка - одна запись, внутри любые делиметеры (можно запятую, можно вертикальную палочку) по очереди поля
 
@@ -109,27 +122,26 @@ class Poo {
 // А весь этот import/export блок можешь показывать по нажатию кнопки Data operations
 // Нажимаем один раз - появляется текстовое поле с кнопками, еще раз - убирается
 
+// static async savePoo() {
+//     const response = await fetch('suitcase.txt', {
+//         method: 'PUT',
+//         headers: {'Content-type': 'application/json'},
+//         body: '7,2,7,3,7,7,7'
+//     })
+//     const responseText = response.text();
+//     responseText
+//     .then(data => console.log(data + 'saved'))
+// }
 
-    // static async savePoo() {
-    //     const response = await fetch('suitcase.txt', {
-    //         method: 'PUT',
-    //         headers: {'Content-type': 'application/json'},
-    //         body: '7,2,7,3,7,7,7'
-    //     })
-    //     const responseText = response.text();
-    //     responseText
-    //     .then(data => console.log(data + 'saved'))
-    // }
+// static async loadPoo() {
+//     putin.poops = [];
+//     document.querySelector('#pooList').innerHTML= ""
+//     await fetch('suitcase.txt')
+//     .then(res => res.text())
+//     .then(data => {
+//         const pooArray = data.split(',')
+//         pooArray.forEach(el => {putin.generatePoo(parseInt(el))
+//         Putin.noPooCheck()})
+//     })
 
-    // static async loadPoo() {
-    //     putin.poops = [];
-    //     document.querySelector('#pooList').innerHTML= ""
-    //     await fetch('suitcase.txt')
-    //     .then(res => res.text())
-    //     .then(data => {
-    //         const pooArray = data.split(',')
-    //         pooArray.forEach(el => {putin.generatePoo(parseInt(el))
-    //         Putin.noPooCheck()})
-    //     })
-    
-    // }
+// }
---
+++ putinPooper/index.js
@@ -0,0 +1,79 @@
+const PORT = 3000;
+let letter;
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./poo.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    return console.log(err.message);
+  }
+  console.log("connected to poo.db");
+});
+
+const checkSuitcase = "SELECT * FROM pooDB";
+db.all(checkSuitcase, [], (err, content) => {
+  if (err) {
+    return console.log(err.message);
+  }
+  console.log(content);
+});
+
+//   const createSuitcase =
+//     "CREATE TABLE pooDB(pooID INTEGER PRIMARY KEY,pooType INTEGER)";
+//   db.run(createSuitcase);
+
+// db.close((err) => {
+//   if (err) {
+//     return console.log(err.message);
+//   }
+//   console.log("connection closed");
+// });
+
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(PORT);
+
+app.get("/", (req, res) => {
+  //   res.sendStatus(200); //не обязательно
+  //   res.send("hi");
+  //   res.status(200).send("privet");
+  //   res.download('index.js')
+  //   res.status(200).json({ message: "privet" });
+  //   res.render("index", { variable: "value" });
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+app.get("/pupoo.jpg", (req, res) => {
+  res.sendFile(path.join(__dirname, "/pupoo.jpg"));
+});
+app.get("/pupoofavicon.ico", (req, res) => {
+  res.sendFile(path.join(__dirname, "/pupoofavicon.ico"));
+});
+app.get("/app.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "/app.js"));
+});
+app.get("/poops", (req, res) => {
+  db.all(checkSuitcase, [], (err, content) => {
+    if (err) {
+      return console.log(err.message);
+    }
+    letter = JSON.stringify(content);
+    res.send(letter);
+  });
+});
+
+app.post("/poop", (req, res) => {
+  console.log(req.body);
+  const addPoo = "INSERT INTO pooDB(pooType)VALUES(?)";
+  db.run(addPoo, [req.body.pooType], (err) => {
+    if (err) {
+      return console.log(err.message);
+    }
+  });
+});
+
+app.post("/reset", (req, res) => {
+  db.run("DELETE FROM pooDB");
+});
